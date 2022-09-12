20c583cc0fa820452c480c4c77a21b0141b7cdd5
Trofimov_AP <trofimov_ap@e-sbyt.ru>
2022-09-12 17:14:35 +0900

    #9 15:00

--- women/models.py
+++ women/models.py
@@ -3,7 +3,10 @@ from django.urls import reverse
 
 
 class Category(models.Model):
-    name = models.CharField()
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Women(models.Model):
@@ -13,6 +16,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    cat = models.ForeignKey(Category, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.title
