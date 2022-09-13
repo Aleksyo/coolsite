c3dec5ef089840f8ec5e71115c4847aa7766bdb1
Trofimov_AP <trofimov_ap@e-sbyt.ru>
2022-09-13 15:50:35 +0900

    #10

--- women/models.py
+++ women/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField('Категория', max_length=100, db_index=True)
 
     def __str__(self):
         return self.name
@@ -11,18 +11,28 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('category', kwargs={'cat_id': self.pk})
 
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['pk']
+
 
 class Women(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey(Category, on_delete=models.PROTECT)
+    title = models.CharField('Заголовок', max_length=255)
+    content = models.TextField('Текст статьи', blank=True)
+    photo = models.ImageField('Фото', upload_to='photos/%Y/%m/%d/')
+    time_create = models.DateTimeField('Время создания', auto_now_add=True)
+    time_update = models.DateTimeField('Время изменения', auto_now=True)
+    is_published = models.BooleanField('Публикация', default=True)
+    cat = models.ForeignKey(Category, on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', kwargs={'post_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Известные женщины'
+        verbose_name_plural = 'Известные женщины'
+        ordering = ['time_create', 'title']
--- women/views.py
+++ women/views.py
@@ -9,7 +9,7 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 
 
 def index(request):
-    posts = Women.objects.all()
+    posts = Women.objects.filter(is_published=True)
     cats = Category.objects.all()
     context = {'posts': posts,
                'cats': cats,
@@ -42,7 +42,7 @@ def show_post(request, post_id):
 
 
 def show_category(request, cat_id):
-    posts = Women.objects.filter(cat_id=cat_id)
+    posts = Women.objects.filter(cat_id=cat_id, is_published=True)
     cats = Category.objects.all()
 
     if not len(posts):
